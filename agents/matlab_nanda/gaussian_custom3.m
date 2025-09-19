abf664eb196b43302c25334ee4f090006fff6053
Deborah Yuen <deborahyuen2016@berkeley.edu>
2025-09-19 01:37:11 -0700

    update gaussian source and function

--- agents/matlab_nanda/gaussian_custom3.m
+++ agents/matlab_nanda/gaussian_custom3.m
@@ -1,15 +1,18 @@
 // Source: https://www.mathworks.com/matlabcentral/fileexchange/110900-step-by-step-gaussian-elimination-method?s_tid=FX_rc3_behav
-[m,n]=size(M);
-for j=1:m-1
-    for z=j+1:m
-        if M(j,j)==0   
-            disp("replace row"+j + " with row"+z)
-            tmp=M(j,:); M(j,:)=M(z,:); 
-            M(z,:)=tmp;
+function M = gaussian_custom3(M)
+    [m,n] = size(M);
+    for j = 1:m-1
+        for z = j+1:m
+            if M(j,j) == 0
+                disp("replace row" + j + " with row" + z);
+                tmp = M(j,:); 
+                M(j,:) = M(z,:); 
+                M(z,:) = tmp;
+            end
         end
-    end
-    for i=j+1:m   
-        disp("Eliminate M("+i+","+j+")")
-        M(i,:)=M(i,:)-M(j,:)*(M(i,j)/M(j,j))
+        for i = j+1:m
+            disp("Eliminate M(" + i + "," + j + ")");
+            M(i,:) = M(i,:) - M(j,:)*(M(i,j)/M(j,j));
+        end
     end
 end
